12853b1dabe7bcdf205c58f49c2ec4e07a39105d
mausayao <mauricio.sayao@gmail.com>
2021-02-21 12:08:42 -0400

    feature: add função para pegar o valor do slider e do segmented control

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
+enum TextField: Int {
+    case noun, adjective, verb
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var textFieldsCollection: [UITextField]!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    
+    let storyPrompt = StoryPromptEntry()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let storyPrompt = StoryPromptEntry()
+        numberSlider.value = 7.5
+       
     }
 
-
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+        
+        print(storyPrompt.genre)
+    }
 }
 
