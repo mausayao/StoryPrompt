0729da22f0371a92ba8f0d560e6c643df4d3b344
mausayao <mauricio.sayao@gmail.com>
2021-02-21 12:32:59 -0400

    feature: botão para gerar story prompt

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -11,33 +11,53 @@ enum TextField: Int {
     case noun, adjective, verb
 }
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet var textFieldsCollection: [UITextField]!
-    @IBOutlet weak var numberLabel: UILabel!
-    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet private var textFieldsCollection: [UITextField]!
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var numberSlider: UISlider!
     
     let storyPrompt = StoryPromptEntry()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         numberSlider.value = 7.5
-       
+    }
+    
+    private func updateStoryPrompt() {
+        storyPrompt.noun = textFieldsCollection[TextField.noun.rawValue].text ?? .init()
+        
+        storyPrompt.adjective = textFieldsCollection[TextField.adjective.rawValue].text ?? .init()
+        
+        storyPrompt.verb = textFieldsCollection[TextField.verb.rawValue].text ?? .init()
     }
 
-    @IBAction func changeNumber(_ sender: UISlider) {
+    @IBAction private func generateStoryPrompt(_ sender: UIButton) {
+        updateStoryPrompt()
+        print(storyPrompt)
+    }
+    
+    @IBAction private func changeNumber(_ sender: UISlider) {
         numberLabel.text = "Number: \(Int(sender.value))"
         storyPrompt.number = Int(sender.value)
     }
     
-    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+    @IBAction private func changeStoryType(_ sender: UISegmentedControl) {
         if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
             storyPrompt.genre = genre
         } else {
             storyPrompt.genre = .scifi
         }
-        
-        print(storyPrompt.genre)
     }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        updateStoryPrompt()
+        return true
+    }
+    
 }
 
